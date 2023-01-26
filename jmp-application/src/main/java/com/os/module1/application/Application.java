252c3ac4372833c5623f3ef5c9e99f08ba85d65a
Sergeii971 <sergeiverbovskiy4@gmail.com>
2023-01-26 18:54:36 +0300

    complete task 9-16

--- jmp-application/src/main/java/com/os/module1/application/Application.java
+++ jmp-application/src/main/java/com/os/module1/application/Application.java
@@ -2,9 +2,10 @@ package com.os.module1.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = {"com.os.module1"})
 public class Application {
     public static void main(String[] args) {
